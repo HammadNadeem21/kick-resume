4f0de34063d4edd3b6e80c421f5fd1344bb33995
HammadNadeem21 <nhammad424@gmail.com>
2025-05-17 12:53:42 +0500

    updated

--- kick-resume/src/app/page.tsx
+++ kick-resume/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import Features from "../components/Features";
 import Security from "@/components/Security";
-import FileUploaderSection from "@/components/FileUploaderSection";
+// import FileUploaderSection from "@/components/FileUploaderSection";
 import ToolBoxSection from "@/components/ToolBoxSection";
 import QuestionSection from "@/components/QuestionSection";
 
