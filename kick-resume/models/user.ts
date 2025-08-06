4a8610e43cec241fb0d3ed8e4379b583d44d11a0
HammadNadeem21 <nhammad424@gmail.com>
2025-08-06 15:47:51 +0500

    updated

--- kick-resume/models/user.ts
+++ kick-resume/models/user.ts
@@ -1,21 +1,5 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  credits: {
-    type: Number,
-    default: 100,
-  },
-});
-
 // const userSchema = new mongoose.Schema({
 //   email: {
 //     type: String,
@@ -24,13 +8,29 @@ const userSchema = new mongoose.Schema({
 //   },
 //   password: {
 //     type: String,
-//     required: false, // 👈 Change this
+//     required: [true, "Password is required"],
 //   },
 //   credits: {
 //     type: Number,
 //     default: 100,
 //   },
 // });
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: false, // 👈 Change this
+  },
+  credits: {
+    type: Number,
+    default: 100,
+  },
+});
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
